9221d579786b3f543eae4149fb52a83982d0d95d
majajobee <mjaubie@gmail.com>
2017-03-19 21:10:28 -0700

    songs and genre models

---
+++ app/views/genres/_genre.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! genre, :id, :name, :picture, :created_at, :updated_at
+json.url genre_url(genre, format: :json)
---
+++ db/migrate/20170320040852_create_songs.rb
@@ -0,0 +1,11 @@
+class CreateSongs < ActiveRecord::Migration[5.0]
+  def change
+    create_table :songs do |t|
+      t.string :name
+      t.string :image
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
